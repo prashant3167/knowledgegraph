9b9e766bfa0e441cd2a7e9168d79cef69e023377
Prashant Gupta <prashantgupta@licious.com>
2023-05-09 11:45:36 +0200

    Property test

--- src/main/java/org/bdm/ontology/ABOX.java
+++ src/main/java/org/bdm/ontology/ABOX.java
@@ -6,10 +6,16 @@ import org.apache.commons.csv.CSVRecord;
 import org.apache.jena.ontology.*;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.util.iterator.ExtendedIterator;
 import org.bdm.utils.Constants;
 import org.bdm.utils.Formatter;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Random;
 
 public class ABOX {
     /**
@@ -18,9 +24,10 @@ public class ABOX {
     public static void loadInstances() throws IOException {
         Model model = ModelFactory.createDefaultModel().read(Constants.TBOX_MODEL_PATH);
         OntModel ontModel = ModelFactory.createOntologyModel( OntModelSpec.RDFS_MEM_RDFS_INF, model);
-
+        System.out.println(ontModel.listClasses());
         //Classes
         OntClass paperClass = ontModel.getOntClass( Constants.BASE_URI.concat("Paper") );
+//        OntClass paperType = ontModel.getOntClass( Constants.BASE_URI.concat("PaperType") );
         OntClass authorClass = ontModel.getOntClass( Constants.BASE_URI.concat("Author") );
         OntClass volumeClass = ontModel.getOntClass( Constants.BASE_URI.concat("Volume") );
         OntClass journalClass = ontModel.getOntClass( Constants.BASE_URI.concat("Journal") );
@@ -29,6 +36,11 @@ public class ABOX {
         OntProperty wrotePaper = ontModel.getOntProperty( Constants.BASE_URI.concat("wrotepaper") );
         OntProperty hasVolume = ontModel.getOntProperty( Constants.BASE_URI.concat("hasvolume") );
         OntProperty publishedIn = ontModel.getOntProperty( Constants.BASE_URI.concat("publishedin") );
+        OntProperty paperType_property = ontModel.getOntProperty( Constants.BASE_URI.concat("papertypeproperty") );
+
+        //ObjectProperty
+//        ObjectProperty test = ontModel.createObjectProperty( Constants.BASE_URI.concat("Type") );
+
 
         //Read output_article.csv
         BufferedReader csvReader = new BufferedReader(new FileReader(Constants.ARTICLES_PATH));
@@ -38,8 +50,27 @@ public class ABOX {
         for(CSVRecord record : parser) {
 
             // Article (paper)
-            String paperTitle = Formatter.format(record.get("title"));
+            String paperTitle = record.get("title").replace(" ", "").replace("\"","");
             Individual paperInd = paperClass.createIndividual(Constants.BASE_URI.concat(paperTitle));
+            Random rand = new Random();
+//            String ptype = Constants.PAPER_TYPE.get(rand.nextInt(Constants.PAPER_TYPE.size()));
+//            Individual paper_type_property = paperType.createIndividual( Constants.BASE_URI.concat( "jjf" ) );
+//            Resource ptype = model.createOntProperty(Constants.BASE_URI.concat("jvjf"));
+//            ObjectProperty lod = ontModel.createObjectProperty(Constants.BASE_URI.concat("hasLevelOfDetail"));
+            System.out.println(ontModel.listOntProperties());
+//            Object println;
+//            System.out.println(ptype);
+            Property property = model.getProperty("http://www.bdma.sdm#type");
+            Property property_2 = model.getProperty("http://www.bdma.sdm#title");
+            paperInd.addProperty(property,"vf");
+            paperInd.addProperty(property_2,record.get("title"));
+
+            ExtendedIterator<OntProperty> it = ontModel.listOntProperties();
+
+            while (it.hasNext()) {
+                OntProperty ontclass = it.next();
+                System.out.println(ontclass.getLocalName());
+            }
 
             // Journal
             String journal = Formatter.format(record.get("journal"));
@@ -76,6 +107,7 @@ public class ABOX {
         FileOutputStream writerStream = new FileOutputStream( Constants.ABOX_MODEL_PATH );
         model.write(writerStream, "N-TRIPLE");
         writerStream.close();
+        System.out.println("I am here");
 
     }
 }
